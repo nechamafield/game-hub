0960b343351f48d18b48379e3a0f2a45e6a309f3
nechamafield <nechamafield@gmail.com>
2024-02-20 18:01:15 -0500

    Get optimized images

---
+++ src/sevices/Image-url.ts
@@ -0,0 +1,7 @@
+const getCroppedImageUrl = (Url: string) => {
+    const target = 'media/';
+    const index = Url.indexOf(target) + target.length;
+    return Url.slice(0, index) + 'crop/600/400/' + Url.slice(index);
+}
+
+export default getCroppedImageUrl;
