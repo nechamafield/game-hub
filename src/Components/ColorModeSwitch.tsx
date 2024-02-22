d1ca3c7c9aa2bcd3cbfc504d7a9853ff7a5d8584
nechamafield <nechamafield@gmail.com>
2024-02-22 16:29:06 -0500

    Build search input

--- src/Components/ColorModeSwitch.tsx
+++ src/Components/ColorModeSwitch.tsx
@@ -5,9 +5,9 @@ const ColorModeSwitch = () => {
     const {toggleColorMode, colorMode} = useColorMode();
 
   return (
-    <HStack>
+    <HStack padding='10px'>
         <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
-        <Text>Dark Mode</Text>
+        <Text whiteSpace='nowrap'>Dark Mode</Text>
     </HStack>
   )
 }
